4475b96fc33e99c5f42ee17e7eba26891aa1269b
Eldar Gabidullin <indiegate@gmail.com>
2017-02-06 11:39:12 +0100

    Show icons

--- src/components/IssueRepository.jsx
+++ src/components/IssueRepository.jsx
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import prIcon from '../icons/stash-pull-request.png';
+import branchIcon from '../icons/stash-branch.png';
+import buildSuccessfulIcon from '../icons/bamboo-build-successful.png';
+import buildFailedIcon from '../icons/bamboo-build-failed.png';
 
 class IssueRepository extends Component {
 
@@ -6,17 +10,35 @@ class IssueRepository extends Component {
     repository: React.PropTypes.object,
   };
 
+  renderRrStatus(status) {
+    return <span>
+      <img src={prIcon} className="PrIcon icon" alt="PR"/>
+      <span className="PrStatus">{status}</span>
+    </span>;
+  }
+
+  renderBranchIcon() {
+    return <img src={branchIcon} className="BranchIcon icon" alt="Branch"/>
+  }
+
+  renderBuildStatus(status) {
+    if (status === 'SUCCESSFUL') {
+      return <img src={buildSuccessfulIcon} className="BuildIcon icon" alt="Build success"/>
+    } else if (status === 'FAILED'){
+      return <img src={buildFailedIcon} className="BuildIcon icon" alt="Build failed"/>
+    }
+    return null;
+  }
 
   renderBranchRow(branchCombined) {
     return (
-      <div>
+      <div className="BranchRow">
         {/*<span>{branchCombined.branchName}</span>*/}
-        <span> {branchCombined.branches ? branchCombined.branches.map(b => <span>[b]</span>) : null}</span>
-        <span> {branchCombined.pullRequests ? branchCombined.pullRequests.map(pr => <span>[pr {pr.status}]</span>) : null}</span>
+        <span> {branchCombined.branches ? branchCombined.branches.map(b => this.renderBranchIcon()) : null}</span>
+        <span> {branchCombined.pullRequests ? branchCombined.pullRequests.map(pr => <span>{this.renderRrStatus(pr.status)}</span>) : null}</span>
       </div>
     );
   }
-
 
   render() {
     const { name, combined, lastCommitBuild } = this.props.repository;
@@ -26,7 +48,7 @@ class IssueRepository extends Component {
           {name}
         </div>
         <div>
-          {lastCommitBuild}
+          {this.renderBuildStatus(lastCommitBuild)}
         </div>
         <div>
           { combined.map(branchCombined => this.renderBranchRow(branchCombined)) }
